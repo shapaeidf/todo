cd09500655afe0cb857e56c808a6145d237656d6
shapaeidf <shapaeidf@naver.com>
2024-01-24 14:26:15 +0900

    feature: page layout, todo box base - Change NotFound Page location

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -21,11 +21,11 @@ export function App() {
   return (
     <BrowserRouter>
       <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
+        titleTemplate="%s - Todo"
+        defaultTitle="Todo"
         htmlAttributes={{ lang: i18n.language }}
       >
-        <meta name="description" content="A React Boilerplate application" />
+        <meta name="description" content="A Todo application" />
       </Helmet>
 
       <Routes>
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,25 +1,62 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { NavBar } from 'app/components/NavBar';
-import { Masthead } from './Masthead';
-import { Features } from './Features';
-import { PageWrapper } from 'app/components/PageWrapper';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  //Wrapper : 요소 가운데로
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #eee;
+`;
+
+const Box = styled.div`
+  width: 400px;
+  height: 600px;
+  background: white;
+  box-shadow: 0px 25px 100px -60px rgba(0, 0, 0, 0.18);
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 15px 25px;
+`;
+
+const TodoList = styled.div``;
+
+const TodoItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100%;
+  font-size: 1.2em;
+  border-bottom: 1px solid #eee;
+`;
+
+const TodoCheck = styled.input`
+  margin-right: 15px;
+`;
 
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React Boilerplate application homepage"
-        />
+        <title>HomePage</title>
+        <meta name="description" content="Todo Main App" />
       </Helmet>
-      <NavBar />
-      <PageWrapper>
-        <Masthead />
-        <Features />
-      </PageWrapper>
+      <Wrapper>
+        <Box>
+          <Title>할 일</Title>
+          <TodoList>
+            <TodoItem>
+              <TodoCheck type="checkbox" />
+              나는 투두야
+            </TodoItem>
+          </TodoList>
+        </Box>
+      </Wrapper>
     </>
   );
 }
